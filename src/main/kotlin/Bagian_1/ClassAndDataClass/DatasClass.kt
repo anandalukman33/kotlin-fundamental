9354ce6bb8228ca03500a8ef90c995e9e7cb38cf
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-30 23:46:40 +0700

    Class and data class end

---
+++ src/main/kotlin/Bagian_1/ClassAndDataClass/DatasClass.kt
@@ -0,0 +1,7 @@
+package Bagian_1.ClassAndDataClass
+
+data class DatasClass (
+    var name : String,
+    var age: String,
+    var address : String
+)
---
+++ src/main/kotlin/Bagian_1/ClassAndDataClass/Null.kt
@@ -0,0 +1,8 @@
+package Bagian_1.ClassAndDataClass
+
+class Null () {
+
+    var name: String? = null
+    var age: String? = null
+    var address: String? = null
+}

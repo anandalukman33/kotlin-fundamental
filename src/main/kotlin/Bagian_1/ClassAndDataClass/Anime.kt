464e1df8e262de144095e27d974e5d791006f7f8
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-30 22:10:23 +0700

    Class and data class

---
+++ src/main/kotlin/Bagian_1/ClassAndDataClass/Anime.kt
@@ -0,0 +1,10 @@
+package Bagian_1.ClassAndDataClass
+
+class Anime {
+    fun description (title: String, synopsis: String, episode: String, rating: String) {
+        println("judul yang kamu ketik adalah : $title")
+        println("synopsis yang kamu ketik adalah : $synopsis")
+        println("episode anime yang kamu ketik adalah : $episode")
+        println("rating yang kamu beri adalah : $rating")
+    }
+}
--- src/main/kotlin/Bagian_1/ClassAndDataClass/Main.kt
+++ src/main/kotlin/Bagian_1/ClassAndDataClass/Main.kt
@@ -6,4 +6,12 @@ fun main () {
     lukman.hobi("Hobiku bermain futsal","Hobiku bermain game")
     println()
     println("[ Ini adalah contoh dari pengambilan nilai dari class sample ]")
+
+    val ketikJudul = readLine()
+    val ketikSynopsis = readLine()
+    val ketikEpisode = readLine()
+    val ketikRating = readLine()
+
+    val mAnime = Anime()
+    mAnime.description("$ketikJudul", "$ketikSynopsis", "$ketikEpisode", "$ketikRating")
 }
